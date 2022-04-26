bdbe96ed81bf8874c8ee46af43b06c277f024ed8
Shivansh Pandey <53964760+shivansh1012@users.noreply.github.com>
2022-04-26 12:48:45 +0530

    Shivansh (#7)
    * Dashboard skeleton
    
    * Note Details page
    
    * Sign in and Sign up pages
    
    * Topic tags, classroom page and home screen update
    
    * Introdutction to firebase
    
    * Minor Updates to Navigator Views
    
    Co-authored-by: Gaurav Gupta <guptagaurav035@gmail.com>

--- smartnotes/lib/main.dart
+++ smartnotes/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'package:smartnotes/screens/main_screen.dart';
+import 'package:smartnotes/views/mobile_view/mobile_view.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -15,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Smart Notes',
       debugShowCheckedModeBanner: false,
-      home: const MainScreen(),
+      home: const MobileView(),
       // home: const MainScreen(),
       theme: ThemeData(
         textButtonTheme: TextButtonThemeData(
--- smartnotes/lib/screens/sign_up.dart
+++ smartnotes/lib/screens/sign_up.dart
@@ -1,11 +1,123 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:smartnotes/constants.dart';
+import 'package:smartnotes/models/user_model.dart';
+import 'package:smartnotes/screens/dashboard.dart';
+import 'package:smartnotes/screens/sign_in.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
 
   @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  //firebase
+  final _auth = FirebaseAuth.instance;
+
+  @override
   Widget build(BuildContext context) {
+    final nameField = TextFormField(
+      autofocus: false,
+      controller: _nameController,
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (value) {
+        _nameController.text = value!;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Name cannot be empty");
+        }
+        if (!RegExp(r"^.{3,}$").hasMatch(value)) {
+          return ("Enter Valid Name(Min. 3 Character)");
+        }
+        return null;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        labelText: "Name",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (value) {
+        _emailController.text = value!;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter your email");
+        }
+        //regex
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Enter a valid email");
+        }
+        return null;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        labelText: "Email ID",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: _passwordController,
+      onSaved: (value) {
+        _passwordController.text = value!;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        if (!RegExp(r"^.{6,}$").hasMatch(value)) {
+          return ("Enter Valid Password(Min. 6 Character)");
+        }
+        return null;
+      },
+      obscureText: true,
+      textInputAction: TextInputAction.done,
+      keyboardType: TextInputType.visiblePassword,
+      decoration: InputDecoration(
+        labelText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+
+    final signUpButton = OutlinedButton(
+      child: const Text(
+        "Sign Up",
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Colors.white,
+        ),
+      ),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(primary),
+      ),
+      onPressed: () {
+        _signUpWithEmailAndPassword();
+      },
+    );
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
@@ -15,110 +127,143 @@ class SignUp extends StatelessWidget {
               margin: const EdgeInsets.symmetric(
                 horizontal: 30.0,
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.green,
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  Text(
-                    "Sign Up",
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      color: primary,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.green,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  TextFormField(
-                    textInputAction: TextInputAction.next,
-                    decoration: InputDecoration(
-                      labelText: "Name",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                    const SizedBox(
+                      height: 30.0,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  TextFormField(
-                    textInputAction: TextInputAction.next,
-                    decoration: InputDecoration(
-                      labelText: "Email ID",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  // Password
-                  TextFormField(
-                    obscureText: true,
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  OutlinedButton(
-                    child: const Text(
+                    Text(
                       "Sign Up",
                       style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.white,
+                        fontSize: 22.0,
+                        color: primary,
                       ),
                     ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(primary),
+                    const SizedBox(
+                      height: 30.0,
                     ),
-                    onPressed: () {
-                      print("Working");
-                    },
-                  ),
-                  const Divider(
-                    color: Colors.black,
-                  ),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  const Center(
-                    child: Text(
-                      "Already have an account?",
-                      style: TextStyle(color: Colors.grey, fontSize: 15.0),
+                    nameField,
+                    const SizedBox(
+                      height: 15.0,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  OutlinedButton(
-                    child: const Text(
-                      "Sign In",
-                      style: TextStyle(fontSize: 16.0),
+                    emailField,
+                    const SizedBox(
+                      height: 15.0,
                     ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  )
-                ],
+                    // Password
+                    passwordField,
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    signUpButton,
+                    const Divider(
+                      color: Colors.black,
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    // const Center(
+                    //   child: Text(
+                    //     "Already have an account?",
+                    //     style: TextStyle(color: Colors.grey, fontSize: 15.0),
+                    //   ),
+                    // ),
+                    // const SizedBox(
+                    //   height: 10.0,
+                    // ),
+                    // OutlinedButton(
+                    //   child: const Text(
+                    //     "Sign In",
+                    //     style: TextStyle(fontSize: 16.0),
+                    //   ),
+                    //   onPressed: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //         builder: (context) => const SignIn(),
+                    //       ),
+                    //     );
+                    //   },
+                    // )
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text("Already have an account? "),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const SignIn()));
+                          },
+                          child: const Text(
+                            "SignIn",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400, fontSize: 16.0),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _signUpWithEmailAndPassword() async {
+    if (_formKey.currentState!.validate()) {
+      await _auth
+          .createUserWithEmailAndPassword(
+              email: _emailController.text, password: _passwordController.text)
+          .then((value) => {
+                saveDetailsToFirestore()
+              })
+          .catchError((e) => {Fluttertoast.showToast(msg: e!.message)});
+    }
+  }
+
+  void saveDetailsToFirestore() async {
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    User? user = _auth.currentUser;
+
+    UserModel userModel = UserModel();
+
+    userModel.name = _nameController.text;
+    userModel.email = user!.email;
+    userModel.uid = user.uid;
+
+    await firebaseFirestore
+        .collection("users")
+        .doc(user.uid)
+        .set(userModel.toMap());
+
+    Fluttertoast.showToast(msg: "Account Created Successfully");
+
+    Navigator.replace(context,
+        oldRoute: MaterialPageRoute(builder: (context) => const SignUp()),
+        newRoute: MaterialPageRoute(builder: (context) => const Dashboard()));
+    // Navigator.pushAndRemoveUntil(
+    //     (context),
+    //     MaterialPageRoute(builder: (context) => const Dashboard()),
+    //     (route) => false);
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 }
