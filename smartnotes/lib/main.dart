a3a1f48974ab3aa2c5f5446bd812d5bb1641ff88
Shivansh Pandey <shivansh.spandey@gmail.com>
2022-04-29 22:06:14 +0530

    New Updated and Workable Bottom Navigation

--- smartnotes/lib/main.dart
+++ smartnotes/lib/main.dart
@@ -1,59 +1,49 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:smartnotes/screens/SplashScreen/splash_screen.dart';
 // import 'package:smartnotes/views/mobile_view/mobile_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // options: const FirebaseOptions(
-    //     apiKey: "AIzaSyAJJolI6XzawYLR1heg3hzanWP-35NuIQ8",
-    //     projectId: "smartnotes-46d96",
-    //     messagingSenderId: "486764833620",
-    //     appId: "1:486764833620:web:11a35126d22886295af6c1"),
-  );
+      // options: const FirebaseOptions(
+      //     apiKey: "AIzaSyAJJolI6XzawYLR1heg3hzanWP-35NuIQ8",
+      //     projectId: "smartnotes-46d96",
+      //     messagingSenderId: "486764833620",
+      //     appId: "1:486764833620:web:11a35126d22886295af6c1"),
+      );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // To fix keyboard error
+  @override
+  void initState() {
+    SystemChannels.textInput.invokeMethod('TextInput.hide');
+    super.initState();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return DismissKeyboard(
-      child: MaterialApp(
-        // showPerformanceOverlay: true,
-        title: 'Smart Notes',
-        debugShowCheckedModeBanner: false,
-        home: const SplashScreen(),
-        theme: ThemeData(
-          textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(primary: const Color(0xff0081C6)),
-          ),
+    return MaterialApp(
+      // showPerformanceOverlay: true,
+      title: 'Smart Notes',
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+      theme: ThemeData(
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(primary: const Color(0xff0081C6)),
         ),
       ),
     );
   }
 }
-
-//https://www.kindacode.com/article/flutter-dismiss-keyboard-when-tap-outside-text-field/
-class DismissKeyboard extends StatelessWidget {
-  final Widget child;
-  const DismissKeyboard({Key? key, required this.child}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScopeNode currentFocus = FocusScope.of(context);
-        if (!currentFocus.hasPrimaryFocus &&
-            currentFocus.focusedChild != null) {
-          FocusManager.instance.primaryFocus?.unfocus();
-        }
-      },
-      child: child,
-    );
-  }
-}
-
--- smartnotes/lib/views/mobile_view/mobile_view.dart
+++ smartnotes/lib/views/mobile_view/mobile_view.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:smartnotes/constants.dart';
 import 'package:smartnotes/models/user_model.dart';
 
 import 'package:smartnotes/screens/Home/home.dart';
@@ -10,39 +9,63 @@ import 'package:smartnotes/screens/Explore/explore.dart';
 import 'package:smartnotes/screens/Forums/forum.dart';
 // import 'package:smartnotes/screens/PersonalNotes/personal_notes.dart';
 import 'package:smartnotes/screens/Profile/profile.dart';
-import 'package:smartnotes/screens/Authentication/sign_in.dart';
 import 'package:smartnotes/screens/Upload/upload.dart';
+import 'package:smartnotes/views/mobile_view/bottom_navigation.dart';
+import 'package:smartnotes/views/mobile_view/tab_item.dart';
 
 class MobileView extends StatefulWidget {
   const MobileView({Key? key}) : super(key: key);
 
   @override
-  State<MobileView> createState() => _MobileViewState();
+  State<MobileView> createState() => MobileViewState();
 }
 
-class _MobileViewState extends State<MobileView> {
-  int currentSelectedPage = 0;
+class MobileViewState extends State<MobileView> {
+  static int currentSelectedPage = 0;
   UserModel loggedInUser = UserModel();
 
-  final List<Widget> pages = [
-    const Home(),
-    const Explore(),
-    const Upload(),
-    const Forum(),
-    // const PersonalNotes(),
-    const SignIn()
+  final List<TabItem> tabs = [
+    TabItem(
+        tabName: "Home",
+        icon: Icons.space_dashboard_rounded,
+        page: const Home()),
+    TabItem(
+        tabName: "Explore",
+        icon: Icons.explore_outlined,
+        page: const Explore()),
+    TabItem(
+        tabName: "Upload",
+        icon: Icons.add_circle_outline_outlined,
+        page: const Upload()),
+    TabItem(
+        tabName: "Forum",
+        icon: Icons.cast_for_education_rounded,
+        page: const Forum()),
+    TabItem(
+        tabName: "Profile",
+        icon: Icons.account_circle_outlined,
+        page: const Profile())
   ];
 
   void onTap(int index) {
-    setState(() {
-      currentSelectedPage = index;
+    if (index == currentSelectedPage) {
+      tabs[index].key.currentState?.popUntil((route) => route.isFirst);
+    } else {
+      setState(() {
+        currentSelectedPage = index;
+      });
+    }
+  }
+
+  MobileViewState() {
+    tabs.asMap().forEach((index, details) {
+      details.setIndex(index);
     });
   }
 
   @override
   void initState() {
     super.initState();
-    // Fluttertoast.showToast(msg: "Main View Initilized");
     User? user = FirebaseAuth.instance.currentUser;
     if (FirebaseAuth.instance.currentUser == null) {
       Fluttertoast.showToast(msg: "No User");
@@ -56,10 +79,13 @@ class _MobileViewState extends State<MobileView> {
       loggedInUser = UserModel.fromMap(value.data());
       Fluttertoast.showToast(msg: "Welcome " + loggedInUser.name.toString());
 
-      // pages.
-      setState(() {
-        pages[4] = const Profile();
-      });
+      // setState(() {
+      //   tabs[4] = TabItem(
+      //       tabName: "Profile", icon: Icons.account_circle_outlined, page: const Profile());
+      //   tabs.asMap().forEach((index, details) {
+      //     details.setIndex(index);
+      //   });
+      // });
     }).catchError((error) {
       Fluttertoast.showToast(msg: error);
     });
@@ -67,89 +93,28 @@ class _MobileViewState extends State<MobileView> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab =
+            !await tabs[currentSelectedPage].key.currentState!.maybePop();
+        // if (isFirstRouteInCurrentTab) {
+        //   if (currentSelectedPage != 0) {
+        //     onTap(0);
+        //     return false;
+        //   }
+        // }
+        return isFirstRouteInCurrentTab;
+      },
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 1,
-          title: const Text(
-            'Smart Notes',
-            style: TextStyle(
-              fontSize: 30.0,
-              color: Colors.black,
-              fontFamily: 'LobsterTwo',
-            ),
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: IconButton(
-                onPressed: () => {
-                  Fluttertoast.showToast(msg: "Personal Notes clicked"),
-                },
-                icon: const Icon(
-                  Icons.article_outlined,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ],
-        ),
-        // body: pages[currentSelectedPage],
+        // indexed stack shows only one child
         body: IndexedStack(
           index: currentSelectedPage,
-          children: pages,
+          children: tabs.map((e) => e.page).toList(),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: (onTap),
-          currentIndex: currentSelectedPage,
-
-          selectedItemColor: primaryLight,
-          // showSelectedLabels: false,
-          // selectedFontSize: 0,
-
-          unselectedItemColor: Colors.black.withOpacity(0.32),
-          // showUnselectedLabels: false,
-          // unselectedFontSize: 0,
-
-          elevation: 0,
-
-          type: BottomNavigationBarType.fixed,
-
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(
-                Icons.space_dashboard_rounded,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: 'Explore',
-              icon: Icon(
-                Icons.explore_outlined,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: 'Upload',
-              icon: Icon(
-                Icons.add_circle_outline_outlined,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: 'Forums',
-              icon: Icon(
-                Icons.cast_for_education_rounded,
-              ),
-            ),
-            // BottomNavigationBarItem(
-            //     label: 'Notes', icon: Icon(Icons.notes_rounded,),),
-            BottomNavigationBarItem(
-              label: 'Profile',
-              icon: Icon(
-                Icons.account_circle_outlined,
-              ),
-            ),
-          ],
+        // Bottom navigation
+        bottomNavigationBar: BottomNavigation(
+          onSelectTab: onTap,
+          tabs: tabs,
         ),
       ),
     );
